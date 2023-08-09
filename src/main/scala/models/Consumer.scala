e22dc56652299d1137adfc5c4a4736de444fbf27
Ichwan Palongengi <ipalongengi@gmail.com>
2023-08-09 14:22:08 -0400

    add additional class of consumer for couple/family

--- src/main/scala/models/Consumer.scala
+++ src/main/scala/models/Consumer.scala
@@ -3,10 +3,16 @@ package models
 sealed trait Consumer
 case object Shared extends Consumer
 case class Individual(name: String) extends Consumer
+case class Group(name: String, individuals: List[Individual]) extends Consumer
 
 object Consumer {
   def isShared(x: Consumer): Boolean = x match {
     case Shared => true
+    case _ => false
+  }
+
+  def isGroup(x: Consumer): Boolean = x match {
+    case _: Group => true
     case _ => false
   }
 }
